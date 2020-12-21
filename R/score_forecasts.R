86fa2a8370563b3308b931ec9acfeee2decbf112
Nicholas Reich <nick@schoolph.umass.edu>
2020-12-21 11:24:53 -0500

    adds "sharpness", "overprediction", and "underprediction" to computed scores.
    closes #87

--- R/score_forecasts.R
+++ R/score_forecasts.R
@@ -15,11 +15,26 @@
 #' Other columns will contain scores: 
 #'  - `abs_error` is the absolute error based on median estimate,
 #'  - `wis` is the weighted interval score
+#'  - `sharpness` the component of WIS made up of interval widths
+#'  - `overprediction` the component of WIS made up of overprediction of intervals
+#'  - `underprediction` the component of WIS made up of underprediction of intervals
 #'  - `coverage_X` are prediction interval coverage at alpha level X
 #' If return_format is "long", also contains columns score_name and score_value
 #' where score_name is the type of score calculated and score_value has the numeric
 #' value of the score.
 #' If return_format is "wide", each calculated score is in its own column.
+#'
+#' @examples
+#' \dontrun{
+#' forecasts <- load_latest_forecasts(models=c("COVIDhub-ensemble", "UMass-MechBayes"), 
+#'   last_forecast_date = "2020-12-14", 
+#'   forecast_date_window_size = 7, 
+#'   locations = c("US"), 
+#'   targets = paste(1:4, "wk ahead inc death"), 
+#'   source = "zoltar")
+#' truth <- load_truth("JHU", target_variable = "inc death", locations = "US")
+#' scores <- score_forecasts(forecasts, truth)
+#' }
 #'
 #' @export
 score_forecasts <- function(
@@ -86,12 +101,23 @@ score_forecasts <- function(
     interval_score_arguments = list(weigh = TRUE)) %>%
     tidyr::pivot_wider(id_cols = observation_cols,
       names_from = c("range"), 
-      values_from = c("coverage", "interval_score", "aem")) %>%
+      values_from = c("coverage", "interval_score", "aem", "sharpness", "overprediction", "underprediction")) %>%
     ## before next line: do we need to check to ensure aem_0 is a valid column name
     dplyr::rename(abs_error = aem_0) %>% 
     ## before next line: do we need to check to ensure interval_score columns exist?
-    dplyr::mutate(wis = rowMeans(dplyr::select(., dplyr::starts_with("interval_score")))) %>%
-    dplyr::select(-dplyr::starts_with("aem_"), -dplyr::starts_with("interval_score")) 
+    dplyr::mutate(
+      wis = rowMeans(dplyr::select(., dplyr::starts_with("interval_score"))),
+      sharpness = rowMeans(dplyr::select(., dplyr::starts_with("sharpness"))),
+      overprediction = rowMeans(dplyr::select(., dplyr::starts_with("overprediction"))),
+      underprediction = rowMeans(dplyr::select(., dplyr::starts_with("underprediction")))
+    ) %>%
+    dplyr::select(
+      -dplyr::starts_with("aem_"), 
+      -dplyr::starts_with("interval_score"), 
+      -dplyr::starts_with("sharpness_"), 
+      -dplyr::starts_with("underprediction_"), 
+      -dplyr::starts_with("overprediction_")
+    ) 
   
   # manipulate return format:
   #   eval_forecasts(), by default, returns in wide format
