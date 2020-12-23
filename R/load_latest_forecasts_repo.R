e7dba4eef985cba6c705d2f0a3268dac1f8b5ab9
Evan Ray <elray@umass.edu>
2020-12-23 12:15:49 -0500

    refactor load_latest_forecasts_repo, splitting out new function load_forecast_files_repo

--- R/load_latest_forecasts_repo.R
+++ R/load_latest_forecasts_repo.R
@@ -78,7 +78,8 @@ load_latest_forecasts_repo <- function(file_path, models, forecast_dates,
   
   forecast_dates <- as.Date(forecast_dates)
   
-  forecasts <- purrr::map_dfr(
+  # get paths to all forecast files
+  forecast_files <- purrr::map_chr(
     models,
     function(model) {
       if (substr(file_path, nchar(file_path), nchar(file_path)) == "/") {
@@ -93,40 +94,111 @@ load_latest_forecasts_repo <- function(file_path, models, forecast_dates,
       
       if (length(results_path) == 0) {
         return(NULL)
+      } else {
+        return(results_path)
       }
-      
-      readr::read_csv(results_path,
-                      col_types = readr::cols(
-                        forecast_date = readr::col_date(format = ""),
-                        target = readr::col_character(),
-                        target_end_date = readr::col_date(format = ""),
-                        location = readr::col_character(),
-                        type = readr::col_character(),
-                        quantile = readr::col_double(),
-                        value = readr::col_double()
-                      ))%>%
-        dplyr::filter(
-          tolower(type) %in% types,
-          location %in% locations,
-          tolower(target) %in% targets) %>%
-        dplyr::transmute(
-          model = model,
-          forecast_date = forecast_date,
-          location = location,
-          target = tolower(target),
-          target_end_date = target_end_date,
-          type = type,
-          quantile = quantile,
-          value = value
-        )
     }
-  ) %>%
-    tidyr::separate(target, into=c("horizon","temporal_resolution","ahead","target_variable"),
-                    remove = FALSE, extra = "merge") %>%
-    dplyr::select(model, forecast_date, location, horizon, temporal_resolution, 
-                  target_variable, target_end_date, type, quantile, value) %>%
-    dplyr::left_join(covidHubUtils::hub_locations, by=c("location" = "fips"))
+  )
+
+  # read in the forecast files
+  forecasts <- load_forecast_files_repo(forecast_files)
 
   return(forecasts)
   
+}
+
+#' Read in a set of forecast files from a repository
+#'
+#' @param file_paths paths to csv forecast files to read in.  It is expected that
+#' the file names are in the format "*YYYY-MM-DD-<model_name>.csv".
+#' @param locations list of valid fips codes. Defaults to all locations with
+#' available forecasts.
+#' @param types character vector specifying type of forecasts to load: “quantile”
+#' or “point”. Defaults to all types in the forecast file
+#' @param targets character vector of targets to retrieve, for example
+#' c('1 wk ahead cum death', '2 wk ahead cum death'). Defaults to all targets
+#' in the forecast file.
+#'
+#' @return data frame with columns model, forecast_date, location, horizon,
+#' temporal_resolution, target_variable, target_end_date, type, quantile, value,
+#' location_name, population, geo_type, geo_value, abbreviation
+#'
+#' @details The model_name in the file name will be used for the value of the
+#' model column in the return result.
+#'
+#' @export
+load_forecast_files_repo <- function(file_paths,
+                                     locations = NULL,
+                                     types = NULL,
+                                     targets = NULL) {
+  # validate file_paths exist
+  files_exist <- file.exists(file_paths)
+  if (!any(files_exist)) {
+    stop("In load_forecast_files_repo, no files exist at the provided file_paths.")
+  } else if (!all(files_exist)) {
+    warning("In load_forecast_files_repo, at least one file did not exist at the provided file_paths.")
+    file_paths <- file_paths[files_exist]
+  }
+
+  # read in each file
+  all_forecasts <- purrr::map_dfr(
+    file_paths,
+    function(file_path) {
+      # extract model name from file name
+      model <- strsplit(file_path, .Platform$file.sep) %>%
+        `[[`(1) %>%
+        tail(1)
+      date_start_ind <- regexpr("\\d\\d\\d\\d\\-\\d\\d\\-\\d\\d\\-", model)
+      if (date_start_ind == -1) {
+        stop("In load_forecast_files_repo, incorrect file name format: must include date in YYYY-MM-DD format")
+      }
+      model <- substr(model, date_start_ind + 11, nchar(model) - 4)
+
+      single_forecast <- readr::read_csv(
+          file_path,
+          col_types = readr::cols(
+            forecast_date = readr::col_date(format = ""),
+            target = readr::col_character(),
+            target_end_date = readr::col_date(format = ""),
+            location = readr::col_character(),
+            type = readr::col_character(),
+            quantile = readr::col_double(),
+            value = readr::col_double()
+          ))
+        
+        if (!is.null(types)) {
+          single_forecast <- single_forecast %>%
+            dplyr::filter(tolower(type) %in% tolower(types))
+        }
+        if (!is.null(types)) {
+          single_forecast <- single_forecast %>%
+            dplyr::filter(tolower(location) %in% tolower(locations))
+        }
+        if (!is.null(types)) {
+          single_forecast <- single_forecast %>%
+            dplyr::filter(tolower(target) %in% tolower(targets))
+        }
+        
+        single_forecast <- single_forecast %>%
+          dplyr::transmute(
+            model = model,
+            forecast_date = forecast_date,
+            location = location,
+            target = tolower(target),
+            target_end_date = target_end_date,
+            type = type,
+            quantile = quantile,
+            value = value
+          )
+
+      return(single_forecast)
+    }) %>%
+    tidyr::separate(target,
+      into = c("horizon", "temporal_resolution", "ahead", "target_variable"),
+      remove = FALSE, extra = "merge") %>%
+    dplyr::select(model, forecast_date, location, horizon, temporal_resolution,
+                  target_variable, target_end_date, type, quantile, value) %>%
+    dplyr::left_join(covidHubUtils::hub_locations, by = c("location" = "fips"))
+
+  return(all_forecasts)
 }
