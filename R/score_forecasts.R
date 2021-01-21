db2b56e6c9be15fc653461400980f346dec17ceb
starkari <aestark@umass.edu>
2021-01-21 13:41:17 -0500

    switch pack use_median_as_point validation

--- R/score_forecasts.R
+++ R/score_forecasts.R
@@ -92,8 +92,10 @@ score_forecasts <- function(
   }
   
   # validate use_median_as_point
-  use_median_as_point <- match.arg(use_median_as_point, choices = c(FALSE,TRUE), several.ok = FALSE )
-
+  # match.arg does not like logical input
+  if (!is.element(use_median_as_point, c(FALSE, TRUE))) {
+    use_median_as_point <- "wide"
+  }
   if (use_median_as_point==FALSE && !("point" %in% unique(forecasts$type))){
     stop("Want to use point forecast when scoring but no point forecast in forecast data")
   }
