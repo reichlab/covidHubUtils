1e526a1d75b0e5bfbe775cee0599ed062217959f
nikosbosse <nikosbosse@gmail.com>
2021-03-02 20:58:01 +0100

    solve merge conflict utils file

--- R/utils.R
+++ R/utils.R
@@ -10,7 +10,7 @@ setup_zoltar_connection <- function() {
 }
 
 
-
+# helper function to get the zoltar project URL
 get_zoltar_project_url <- function(hub = c("US", "ECDC"), 
                                    zoltar_connection = NULL) {
   # if no zoltar connection was provided, set up a new one
@@ -48,3 +48,20 @@ join_with_hub_locations <- function(data,
   }
   return(data)
 }
+
+globalVariables(
+  c(".", "County", "Name", "Prediction", "Interval", "State", "StateFIPS", 
+    "V1", "V2", "abbreviation", "alpha", "cases", "cases_deaths", "county", 
+    "countyFIPS", "cum", "deaths", "designation", "endpoint_type", 
+    "exists_interval_score_0", "fips", "forecast_date", "full_location_name", 
+    "geo_type", "head", "horizon", "inc", "inc_cases", "inc_deaths", 
+    "interval_score_0", "location", "location_name", "lower", "model", 
+    "model.x", "model_abbr", "n_interval_scores", "notes", "overprediction_0", 
+    "point", "quantile", "sharpness_0", "state", "state_abbreviation", 
+    "tail", "target", "target_end_date", "target_variable", 
+    "team_model_designation", "temporal_resolution", "timezero", "true_value", 
+    "truth_forecast", "truth_model", "type", "underprediction_0", 
+    "unit", "upper", "value", "value.x", "value.y", 
+    "County", "Name", "Prediction", "Interval"
+  )
+)
