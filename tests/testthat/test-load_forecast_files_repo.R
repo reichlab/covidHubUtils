a30b935d986ada3376ccc4d42f4f3f609c25fd0a
Serena-Wang <wang225y@mtholyoke.edu>
2021-04-19 22:02:03 -0400

    add unit test

--- tests/testthat/test-load_forecast_files_repo.R
+++ tests/testthat/test-load_forecast_files_repo.R
@@ -16,9 +16,34 @@ test_that("load_forecast_files_repo works", {
       dplyr::distinct(model, forecast_date) %>%
       dplyr::arrange(model, forecast_date),
     tidyr::expand_grid(
-      model = c("COVIDhub-baseline", "COVIDhub-ensemble"),
+      model = c("COVIDhub-baseline", "COVIDhub-ensemble", "model-model1"),
       forecast_date = lubridate::ymd(c("2020-12-07", "2020-12-14"))
     )
   )
 })
 
+test_that("drop rows with null in value column",{
+  actual <- covidHubUtils::load_forecast_files_repo(
+    "test-data/test-load_forecasts/data-processed/model-model1/2020-12-07-model-model1.csv")
+  
+  original <- readr::read_csv(
+    "test-data/test-load_forecasts/data-processed/model-model1/2020-12-07-model-model1.csv")
+  
+  # number of rows match
+  expect_true(nrow(original) - nrow(original[is.na(original$value), ]) == nrow(actual))
+  
+  # all NULLs are at rows where location is 60.
+  expect_true(!unique(original[is.na(original$value), ]$location) %in% actual$location)
+  
+})
+
+test_that("works when no need to drop rows",{
+  actual <- covidHubUtils::load_forecast_files_repo(
+    "test-data/test-load_forecasts/data-processed/model-model1/2020-12-14-model-model1.csv")
+  
+  original <- readr::read_csv(
+    "test-data/test-load_forecasts/data-processed/model-model1/2020-12-14-model-model1.csv")
+  
+  # number of rows match
+  expect_true(nrow(original) - nrow(original[is.na(original$value), ]) == nrow(actual))
+})
