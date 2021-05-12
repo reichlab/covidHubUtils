64916d48c3bf43f3bdf2867fe9a9477d1f7fb5cc
Khoale1096 <Khoale1096>
2021-05-12 04:26:51 -0400

    Add method to generate visualization json truth

--- R/get_truth.R
+++ R/get_truth.R
@@ -281,6 +281,94 @@ preprocess_jhu <- function (save_location="./data-truth"){
 }
 
 
+#' Preprocess raw truth data from JHU CSSE into Cumulative/Incident - Deaths/Cases for visualization 
+#' purpose and write to JSON files
+#' 
+#' @param save_location character specifying the location of to save raw truth data.
+#' Default to "./visualization/vis-master/covid-csv-tools/dist/truth"
+#' @export
+preprocess_visualization_truth <- function (save_location="./visualization/vis-master/covid-csv-tools/dist/truth"){
+  # Location name
+  location_names <- covidData::fips_codes[, c("location", "location_name", "abbreviation")]
+  
+  # Spatial resolutions
+  spatial_resolutions <- c("national", "state")
+  
+  # Combine to get cases data
+  cases_dframes <- covidData::load_jhu_data(spatial_resolution = spatial_resolutions,
+                                            temporal_resolution = "weekly",
+                                            measure = "cases")
+  cases_dframes <- dplyr::left_join(cases_dframes, location_names, by = "location")
+  cases_mmwr <- lapply(cases_dframes['date'], MMWRweek::MMWRweek)$date
+  # shift epiweek on axis
+  cases_mmwr_week <- cases_mmwr['MMWRweek'] + 1
+  cases_mmwr_year <- cases_mmwr['MMWRyear']
+  cases_mmwr_year[which(cases_mmwr_week>53),] <- cases_mmwr_year[which(cases_mmwr_week>53),] + 1
+  cases_mmwr_week[which(cases_mmwr_week>53),] <- 1
+  # format date as "{epiyear}{epiweek}". Exp: "202005"
+  cases_mmwr_week <- data.frame(lapply(cases_mmwr_week, sprintf, fmt = "%02d"))
+  cases_dframes['date'] <- paste(cases_mmwr_year$MMWRyear, cases_mmwr_week$MMWRweek, sep = "")
+  # Replace US with "nat" this is NECESSARY for visualization code!
+  cases_dframes <- cases_dframes %>%
+    dplyr::mutate(abbreviation = replace(abbreviation, abbreviation == "US", "nat"))
+  # Threshold to 0 for incident values < 0, for visualization purpose only
+  cases_dframes$inc[cases_dframes$inc < 0] <- 0
+  
+  # Combine to get death data
+  deaths_dframes <- covidData::load_jhu_data(spatial_resolution = spatial_resolutions,
+                                             temporal_resolution = "weekly",
+                                             measure = "deaths")
+  deaths_dframes <- dplyr::left_join(deaths_dframes, location_names, by = "location")
+  deaths_mmwr <- lapply(deaths_dframes['date'], MMWRweek::MMWRweek)$date
+  # shift epiweek on axis
+  deaths_mmwr_week <- deaths_mmwr['MMWRweek'] + 1
+  deaths_mmwr_year <- deaths_mmwr['MMWRyear']
+  deaths_mmwr_year[which(deaths_mmwr_week>53),] <- deaths_mmwr_year[which(deaths_mmwr_week>53),] + 1
+  deaths_mmwr_week[which(deaths_mmwr_week>53),] <- 1
+  # format date as "{epiyear}{epiweek}". Exp: "202005"
+  deaths_mmwr_week <- data.frame(lapply(deaths_mmwr_week, sprintf, fmt = "%02d"))
+  deaths_dframes['date'] <- paste(deaths_mmwr_year$MMWRyear, deaths_mmwr_week$MMWRweek, sep = "")
+  # Replace US with "nat" this is NECESSARY for visualization code!
+  deaths_dframes <- deaths_dframes %>%
+    dplyr::mutate(abbreviation = replace(abbreviation, abbreviation == "US", "nat"))
+  # Threshold to 0 for incident values < 0, for visualization purpose only
+  deaths_dframes$inc[deaths_dframes$inc < 0] <- 0
+  
+  # Get cumulative deaths
+  cumulative_deaths <- deaths_dframes[, c("abbreviation", "date", "cum")]
+  names(cumulative_deaths)[1:3] <- c("location","epiweek","value")
+  cumulative_deaths <- cumulative_deaths[!rowSums(is.na(cumulative_deaths[c("location","value")])), ]
+  cumulative_deaths$value[cumulative_deaths$value == 0] <- 0.1
+  jsonlite::write_json(cumulative_deaths, path = file.path(save_location,"Cumulative Deaths.json"))
+  
+  # Get incident deaths
+  incident_deaths <- deaths_dframes[, c("abbreviation", "date", "inc")]
+  names(incident_deaths)[1:3] <- c("location","epiweek","value")
+  incident_deaths <- incident_deaths[!rowSums(is.na(incident_deaths[c("location","value")])), ]
+  incident_deaths$value[incident_deaths$value == 0] <- 0.1
+  jsonlite::write_json(incident_deaths, path = file.path(save_location,"Incident Deaths.json"))
+  
+  # Get cumulative cases
+  cumulative_cases <- cases_dframes[, c("abbreviation", "date", "cum")]
+  names(cumulative_cases)[1:3] <- c("location","epiweek","value")
+  cumulative_cases <- cumulative_cases[!rowSums(is.na(cumulative_cases[c("location","value")])), ]
+  cumulative_cases$value[cumulative_cases$value == 0] <- 0.1
+  jsonlite::write_json(cumulative_cases, path = file.path(save_location,"Cumulative Cases.json"))
+  
+  # Get incident cases
+  incident_cases <- cases_dframes[, c("abbreviation", "date", "inc")]
+  names(incident_cases)[1:3] <- c("location","epiweek","value")
+  incident_cases <- incident_cases[!rowSums(is.na(incident_cases[c("location","value")])), ]
+  incident_cases$value[incident_cases$value == 0] <- 0.1
+  jsonlite::write_json(incident_cases, path = file.path(save_location,"Incident Cases.json"))
+  
+  return(list("cumulative_deaths" = cumulative_deaths, 
+              "incident_deaths" = incident_deaths,
+              "cumulative_cases" = cumulative_cases, 
+              "incident_cases" = incident_cases))
+}
+
+
 #' Preprocess raw hospitalization data into Cumulative/Incident hospitalizations and write to CSVs
 #' 
 #' @param save_location character specifying the location of to save raw truth data.
