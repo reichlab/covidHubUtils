52ea74e40b7261f0f4d65d6622874912d4b53e7b
Serena-Wang <wang225y@mtholyoke.edu>
2021-07-08 12:45:30 -0400

    update github action and unit test

--- R/aggregate_to_weekly.R
+++ R/aggregate_to_weekly.R
@@ -1,28 +1,31 @@
 #' Aggregate daily data to weekly data
-#' 
+#'
 #' If the last week is not complete, drop all observations from the
 #' previous Saturday in that week
-#' 
+#'
 #' @param data data frame with columns model, location, target_variable, target_end_date and value
-#' 
+#'
 #' @return data frame with the same set of input columns and weekly aggregated data in value column
-#' 
+#'
 #' @export
-aggregate_to_weekly <- function(data){
+aggregate_to_weekly <- function(data) {
   # check if data has all needed columns
-  columns_check <- all(c("model", "target_variable", 
-                         "target_end_date", "location","value") 
-                       %in% colnames(data))
-  if(!columns_check){
+  columns_check <- all(c(
+    "model", "target_variable",
+    "target_end_date", "location", "value"
+  )
+  %in% colnames(data))
+  if (!columns_check) {
     stop("Error in aggregate_to_weekly: Please provide columns model, 
            target_variable, target_end_date, location and value in data.")
-  } 
-  
-  
+  }
+
   data <- data %>%
     dplyr::mutate(
       sat_date = lubridate::ceiling_date(
-        lubridate::ymd(target_end_date), unit = "week") - 1
+        lubridate::ymd(target_end_date),
+        unit = "week"
+      ) - 1
     ) %>%
     dplyr::group_by(model, location, target_variable) %>%
     # if the last week is not complete, drop all observations from the
@@ -36,6 +39,6 @@ aggregate_to_weekly <- function(data){
     dplyr::group_by(model, location, target_end_date, target_variable) %>%
     dplyr::summarize(value = sum(value, na.rm = FALSE)) %>%
     dplyr::ungroup()
-  
+
   return(data)
 }
--- tests/testthat/test-aggregate_to_weekly.R
+++ tests/testthat/test-aggregate_to_weekly.R
@@ -2,59 +2,14 @@ context("aggregate_to_weekly")
 library(covidHubUtils)
 library(dplyr)
 
-test_that("Aggregation works",{
-  actual <- covidData::load_jhu_data(
-    temporal_resolution = 'daily',
-    measure = 'deaths',
-    replace_negatives = FALSE,
-    adjustment_cases = 'none',
-    as_of = '2021-06-02',
-    geography = c("US"),
-    location_code = "US") %>%
-    dplyr::select(-cum) %>%
-    dplyr::rename(value = inc, target_end_date = date)%>%
-    dplyr::mutate(model = "Observed Data (JHU)", target_variable = "inc death")
-  
-  actual <- aggregate_to_weekly(actual)
-  
-  expected <- covidData::load_jhu_data(
-    temporal_resolution = 'weekly',
-    measure = 'deaths',
-    replace_negatives = FALSE,
-    adjustment_cases = 'none',
-    as_of = '2021-06-02',
-    geography = c("US"),
-    location_code = "US") %>%
-    dplyr::select(-cum) %>%
-    dplyr::rename(value = inc, target_end_date = date)%>%
-    dplyr::mutate(model = "Observed Data (JHU)", target_variable = "inc death")
-  
-  data <- merge(actual, expected, by = c("model", "location", "target_end_date", "target_variable"))
-  
-  expect_true(all(data$value.x - data$value.y == 0))
-  
-})
+test_that("Aggregation works", {
+  daily_data <- readr::read_csv("test-data/test-aggregate_to_weekly/test_daily_data.csv")
 
-test_that("load_truth uses aggregation correctly",{ 
-  actual <- load_truth(truth_source = c("JHU"),
-                       target_variable = c("inc death"),
-                       temporal_resolution = "weekly",
-                       locations = "US") %>%
-    dplyr::select(model, target_variable, location, target_end_date, value)
-  
-  expected <- covidData::load_jhu_data(
-    temporal_resolution = 'weekly',
-    measure = 'deaths',
-    replace_negatives = FALSE,
-    adjustment_cases = 'none',
-    geography = c("US"),
-    location_code = "US") %>%
-    dplyr::select(-cum) %>%
-    dplyr::rename(value = inc, target_end_date = date)%>%
-    dplyr::mutate(model = "Observed Data (JHU)", target_variable = "inc death")
-  
+  actual <- aggregate_to_weekly(daily_data)
+
+  expected <- readr::read_csv("test-data/test-aggregate_to_weekly/test_expected_weekly_data.csv")
+
   data <- merge(actual, expected, by = c("model", "location", "target_end_date", "target_variable"))
-  
+
   expect_true(all(data$value.x - data$value.y == 0))
-  
-  })
+})
