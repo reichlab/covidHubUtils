0c318c39654eca9b89aa04336754fd24407cde5b
Hugo Gruson <Bisaloo@users.noreply.github.com>
2022-07-19 19:17:24 +0200

    Revert "Remove redundant select()"
    This reverts commit 53393dc81e0bfa859dfa533b4b26db49a70dba98.

--- R/load_forecasts_repo.R
+++ R/load_forecasts_repo.R
@@ -290,6 +290,10 @@ load_forecast_files_repo <- function(file_paths,
       into = c("horizon", "temporal_resolution", "ahead", "target_variable"),
       sep = " ",  remove = FALSE, extra = "merge"
     ) %>%
+    dplyr::select(
+      model, forecast_date, location, horizon, temporal_resolution,
+      target_variable, target_end_date, type, quantile, value
+    ) %>%
     join_with_hub_locations(hub = hub)
   return(all_forecasts)
 }
