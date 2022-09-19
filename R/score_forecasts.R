531066247c567c24c3be1f614640367f3deb39ba
starkari <aestark@umass.edu>
2022-09-19 12:09:27 -0400

    remove old code

--- R/score_forecasts.R
+++ R/score_forecasts.R
@@ -236,58 +236,6 @@ score_forecasts <- function(
       
     }
     
-    
-    
-    # var_scores <- scoringutils::score(
-    #   data = joint_df_target) %>%
-    #   scoringutils::summarise_scores(by = c(observation_cols, "range")) %>%
-    #   tidyr::pivot_wider(
-    #     id_cols = observation_cols,
-    #     names_from = c("range"),
-    #     values_from = c("coverage", "interval_score", abs_var, "dispersion", "overprediction", "underprediction")
-    #   ) %>%
-    #   purrr::set_names(~ sub(abs_var_rename, "abs_error", .x)) %>%
-    #   ## need to remove all columns ending with NA to not affect WIS calculations
-    #   dplyr::select(
-    #     -dplyr::ends_with("_NA")
-    #   ) %>%
-    #   ## before next lines: do we need to check to ensure interval_score columns exist?
-    #   ## the following lines ensure that we use denominator for the wis of
-    #   ## (# of interval_scores)-0.5
-    #   ## which is written in the paper and elsewhere as
-    #   ## (# of interval_scores at level >0 ) + 0.5 or (K + 1/2)
-    #   ## to make sure that the median only gets half the weight of the other
-    #   ## intervals, multiply its value by 0.5
-    #   dplyr::mutate(
-    #     n_interval_scores = rowSums(!is.na(dplyr::select(., dplyr::starts_with("interval_score")))),
-    #     exists_interval_score_0 = "interval_score_0" %in% names(.),
-    #     interval_score_0 = ifelse(exists_interval_score_0, 0.5 * interval_score_0, NA_real_),
-    #     dispersion_0 = ifelse(exists_interval_score_0, 0.5 * dispersion_0, NA_real_),
-    #     underprediction_0 = ifelse(exists_interval_score_0, 0.5 * underprediction_0, NA_real_),
-    #     overprediction_0 = ifelse(exists_interval_score_0, 0.5 * overprediction_0, NA_real_)
-    #   ) %>%
-    #   dplyr::mutate(
-    #     wis = rowSums(dplyr::select(., dplyr::starts_with("interval_score")), na.rm = FALSE) / (n_interval_scores - 0.5 * (exists_interval_score_0)),
-    #   ) %>%
-    #   dplyr::mutate(
-    #     dispersion = rowSums(dplyr::select(., dplyr::starts_with("dispersion")), na.rm = FALSE) / (n_interval_scores - 0.5 * (exists_interval_score_0)),
-    #     overprediction = rowSums(dplyr::select(., dplyr::starts_with("overprediction")), na.rm = FALSE) / (n_interval_scores - 0.5 * (exists_interval_score_0)),
-    #     underprediction = rowSums(dplyr::select(., dplyr::starts_with("underprediction")), na.rm = FALSE) / (n_interval_scores - 0.5 * (exists_interval_score_0))
-    #   ) %>%
-    #   dplyr::select(
-    #     -dplyr::starts_with("ae_median_"),
-    #     -dplyr::starts_with("ae_point_"),
-    #     -dplyr::starts_with("interval_score"),
-    #     -dplyr::starts_with("dispersion_"),
-    #     -dplyr::starts_with("underprediction_"),
-    #     -dplyr::starts_with("overprediction_")
-    #   ) %>%
-    #   dplyr::select(
-    #     1:8, dplyr::starts_with("coverage_"),
-    #     dplyr::starts_with("abs_error"),
-    #     "n_interval_scores", "exists_interval_score_0", "wis",
-    #     "dispersion", "overprediction", "underprediction"
-    #   )
     scores <- rbind(scores,var_scores)
   }
 
